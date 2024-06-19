70b81c94c574aa9de09936a4cde17e7d10854d10
dorakemon <51844896+dorakemon@users.noreply.github.com>
2024-06-19 16:49:56 +0900

    feat: update opacity

--- app/verifier/page.tsx
+++ app/verifier/page.tsx
@@ -7,7 +7,7 @@ export default () => {
         className="absolute inset-0 w-full h-full object-cover"
       />
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className="max-w-md p-6 bg-white bg-opacity-90 rounded-lg shadow-lg backdrop-blur-sm">
+        <div className="max-w-md p-6 bg-white bg-opacity-80 rounded-lg shadow-lg backdrop-blur-sm">
           <div className="flex flex-col items-center">
             <h1 className="text-3xl font-bold mb-4">図書館QRログイン</h1>
             <p className="text-gray-700 mb-6 text-center">

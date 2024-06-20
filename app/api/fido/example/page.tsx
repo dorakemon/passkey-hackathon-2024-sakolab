2dd911cb56cd06b65ecdb642f171b6acb62d4def
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-06-20 17:02:33 +0900

    add: WebAuthnログインのテスト用ページを作成

---
+++ app/api/fido/example/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import {
+  startAuthentication,
+  startRegistration,
+} from "@simplewebauthn/browser";
+
+export default () => {
+  const handleRegistration = async () => {
+    const response = await fetch("/api/fido/register");
+
+    let registrationResponse;
+    try {
+      const opts = await response.json();
+
+      registrationResponse = await startRegistration(opts);
+      console.log(JSON.stringify(registrationResponse));
+    } catch (error) {
+      throw error;
+    }
+
+    const verificationResponse = await fetch("/api/fido/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(registrationResponse),
+    });
+
+    const verificationJSON = await verificationResponse.json();
+    if (verificationJSON && verificationJSON.verified) {
+      alert("Registration successful");
+    } else {
+      alert("Registration failed");
+    }
+  };
+
+  const handleAuthentication = async () => {
+    const response = await fetch("/api/fido/authenticate");
+
+    let authenticationResponse;
+    try {
+      const opts = await response.json();
+
+      authenticationResponse = await startAuthentication(opts);
+      console.log(JSON.stringify(authenticationResponse));
+    } catch (error) {
+      throw error;
+    }
+
+    const verificationResponse = await fetch("/api/fido/authenticate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(authenticationResponse),
+    });
+
+    const verificationJSON = await verificationResponse.json();
+    if (verificationJSON && verificationJSON.verified) {
+      alert("Authentication successful");
+    } else {
+      alert("Authentication failed");
+    }
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>This is Wallet Home</div>
+
+      <section>
+        <button id="registration" onClick={handleRegistration}>
+          Register
+        </button>
+      </section>
+      <section>
+        <button id="authentication" onClick={handleAuthentication}>
+          Authenticate
+        </button>
+      </section>
+    </main>
+  );
+};
